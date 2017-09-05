89c89b84b753fadb67594b737489d170bc37170f
Marlon O'Neil <hello@marlononeil.com>
2017-09-05 14:26:02 -0400

    performed db:rollback

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170905182145) do
-
-  create_table "parts", force: :cascade do |t|
-    t.string  "name"
-    t.string  "manufacturer"
-    t.decimal "cost"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
