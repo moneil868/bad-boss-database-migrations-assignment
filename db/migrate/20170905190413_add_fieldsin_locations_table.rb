76802b18db560ad9cc2b6eaeae50c886d06934f3
Marlon O'Neil <hello@marlononeil.com>
2017-09-05 15:05:09 -0400

    created and executed migration file to add weather and city columns to Locations table

---
+++ db/migrate/20170905190413_add_fieldsin_locations_table.rb
@@ -0,0 +1,6 @@
+class AddFieldsinLocationsTable < ActiveRecord::Migration[5.0]
+  def change
+    add_column :locations, :city, :string
+    add_column :locations, :weather, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170905190204) do
+ActiveRecord::Schema.define(version: 20170905190413) do
 
   create_table "locations", force: :cascade do |t|
+    t.string "city"
+    t.string "weather"
   end
 
   create_table "parts", force: :cascade do |t|
