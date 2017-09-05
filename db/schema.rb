aaa7fcc40905a1784ce7d06243d4caab51845077
Marlon O'Neil <hello@marlononeil.com>
2017-09-05 14:26:14 -0400

    performed migrate again

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170905182145) do
+
+  create_table "parts", force: :cascade do |t|
+    t.string  "name"
+    t.string  "manufacturer"
+    t.decimal "cost"
+  end
 
 end
