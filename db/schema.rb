ad5e510985673c5f8b291cf03f1135e8f7440694
Marlon O'Neil <hello@marlononeil.com>
2017-09-05 14:52:40 -0400

    created migration to change party_guests table name to widgets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170905184219) do
+ActiveRecord::Schema.define(version: 20170905185018) do
 
   create_table "locations", force: :cascade do |t|
     t.string  "name"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20170905184219) do
     t.integer "quantity"
   end
 
-  create_table "party_guests", force: :cascade do |t|
+  create_table "widgets", force: :cascade do |t|
     t.string  "first_name"
     t.string  "last_name"
     t.string  "dietary_restrictions"
